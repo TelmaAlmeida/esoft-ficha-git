f5b1fb4ac2d88fc3ceb6fb1d378d9c8b55a8de33
Eduardo Andrade <eduardo.andrade@octacode.pt>
2022-03-14 20:45:27 +0000

    Adicionar o meu nome

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,10 +3,13 @@ import javax.swing.*;
 public class HelloWorld extends JFrame {
     private JPanel panel1;
     private JLabel lblMensagem;
+    private JLabel lblMensagem2;
 
     public HelloWorld(){
         setContentPane(panel1);
         pack();
+
+        lblMensagem2.setText("lmao");
     }
     public static void main(String[] args) {
         new HelloWorld().setVisible(true);
