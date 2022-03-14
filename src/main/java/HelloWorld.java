3b0d06203da3d1f5d3366798d743fc7f0fcd62de
Telma Almeida <2212483@my.ipleiria.pt>
2022-03-14 20:58:15 +0000

    add meu nome

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,10 +3,12 @@ import javax.swing.*;
 public class HelloWorld extends JFrame {
     private JPanel panel1;
     private JLabel lblMensagem;
+    private JLabel lblMensagem2;
 
     public HelloWorld(){
         setContentPane(panel1);
         pack();
+        lblMensagem2.setText("telma");
     }
     public static void main(String[] args) {
         new HelloWorld().setVisible(true);
